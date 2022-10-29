63c3c0ee29cba67125a6c1578dc86debe5f01cac
ThanHtutZaw <zgyi5025@gmail.com>
2022-10-29 17:32:03 +0630

    Update firebase.js

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -10,13 +10,7 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyApemEaBzb-V_YAS09BhE5a519xLHYWpSc",
-  authDomain: "todolist-zeee.firebaseapp.com",
-  projectId: "todolist-zeee",
-  storageBucket: "todolist-zeee.appspot.com",
-  messagingSenderId: "429007270792",
-  appId: "1:429007270792:web:7afce2fabb045b66ae403d",
-  measurementId: "G-YS87ZG2Y76"
+  //here
 };
 
 // Initialize Firebase
