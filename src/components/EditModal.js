8c954d7d0a2e5c7f385c512d5589afaac2804d81
ThanHtutZaw <zgyi5025@gmail.com>
2022-11-24 20:53:42 +0630

    css : textarea

--- src/components/EditModal.js
+++ src/components/EditModal.js
@@ -9,6 +9,9 @@ export default function EditModal(props) {
       {todo.id === SelectedID.toString() && 
       <div className="editModal" onClick={(e)=>{e.preventDefault(); e.stopPropagation();}} key={todo.id}>        
           <div className="editInput" ref={editInput} contentEditable>{todo.text}</div>
+          {/* <textarea autoFocus className="editInput" ref={editInput} defaultValue={todo.text}></textarea> */}
+          {/* <div ref={editInput}  contenteditable="true" aria-multiline="true" role="textbox" class="notranslate IZ65Hb-YPqjbf fmcmS-x3Eknd h1U9Be-YPqjbf" dir="ltr" tabindex="0" spellcheck="true">{todo.text}</div> */}
+          
       </div>}
     </>
   )
