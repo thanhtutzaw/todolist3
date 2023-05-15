d575da6f4c73aa8a05e07e087583d0cecc26b51c
thanhtutzaw <zgyi5025@gmail.com>
2023-05-15 09:12:27 +0630

    css : login page

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -9,6 +9,8 @@ import { CSSProperties, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from './Elements/Button/Button';
 import GoogleLogo from './GoogleLogo';
+import Logo from './Logo';
+import { motion } from 'framer-motion';
 export default function Login() {
   const navigate = useNavigate();
   const [loading, setLoading] = useState(false);
@@ -23,10 +25,10 @@ export default function Login() {
     border: '1px solid rgba(0,0,0,.1)',
     cursor: 'pointer',
     boxShadow: '0px 2px 20px #0000001f',
-    position: 'absolute',
+    // position: 'absolute',
     top: '50%',
     right: '50%',
-    transform: 'translate(50%, -50%)',
+    // transform: 'translate(50%, -50%)',
     wordBreak: 'break-word',
     whiteSpace: 'nowrap',
   };
@@ -71,9 +73,11 @@ export default function Login() {
       alert(`Cannot Signin ! ${error.message}`);
     }
   };
-  
+
   return (
-    <main>
+    <motion.main className="login">
+      {/* <image src={"/public/assets/logo512.svg"}/> */}
+      <Logo />
       <Button disabled={loading} onClick={signinHandle} style={loginStyle}>
         <GoogleLogo />
         <p className="loginLabel">{loading ? 'Signing in' : 'Continue with Google'}</p>
@@ -89,7 +93,7 @@ export default function Login() {
       <div
         className="testUserLogin"
         style={{
-          color: EmailLoading ? 'grey' : 'blue',
+          color: EmailLoading ? 'grey' : 'rgb(0, 122, 246)',
           textDecoration: EmailLoading ? 'initial' : 'underline',
           cursor: EmailLoading ? 'progress' : 'pointer',
         }}
@@ -97,6 +101,6 @@ export default function Login() {
       >
         <p>{EmailLoading ? 'Signing in as TestUser ...' : 'Sign in as TestUser'}</p>
       </div>
-    </main>
+    </motion.main>
   );
 }
